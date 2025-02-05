bd74238d5e03638c3fa09d5faf2d1929683d4118
TESLA003 <gokul.vdgs@gmail.com>
2025-02-05 16:58:17 +0530

    nothing just checking

--- For Loop.py
+++ For Loop.py
@@ -2,5 +2,3 @@ for number in range(3):
     print("Attempt", number+1, (number+1)*".")
 for number in range(3, 10):
     print("Attempt", number+1, (number+1)*".")
-for number in range(3, 10, 2):
-    print("Attempt", number+1, (number+1)*".")
